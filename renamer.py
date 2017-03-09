2cf0f814f002dfe7bd8cef4d53475504f90e2aaf
Andrew Childress <carhutt@users.noreply.github.com>
2017-03-08 19:23:23 -0600

    Add files via upload
    adding renamer.py

---
+++ renamer.py
@@ -0,0 +1,51 @@
+# !/usr/bin/python
+
+"""
+This is an example of a way to bulk rename a bunch of files in a directory.
+This one renames a bunch of gdoc files.
+In this example newnames holds all the new names.
+The directory is hardcoded in in the for loop because os.listdir lets you filter
+by file type.
+Question: how to add an extension to the new files?
+"""
+
+import os, sys
+
+newnames = [
+"approach",
+"cities",
+"company",
+"home",
+"industrial",
+"offices",
+"p-buildingautomation",
+"p-citizensafety",
+"p-environment",
+"p-guidance",
+"p-instore",
+"p-inventory",
+"p-network",
+"p-spaceplanning",
+"p-storeops",
+"p-systems",
+"p-tech",
+"p-telco",
+"p-transportation",
+"p-urbaneconomic",
+"partnerships",
+"stores"
+];
+
+index=0
+# listing directories
+print "The dir is: %s"%os.listdir(os.getcwd())
+
+for f in os.listdir("/Users/Andrew/Documents/KHi/Smart Cities/GE/bash_scraper/texts/html/GDrive/"):
+	if f.endswith(".gdoc"):
+		os.rename(f, newnames[index])
+		index += 1
+
+print "Successfully renamed."
+
+# listing directories after renaming "tutorialsdir"
+print "the dir is: %s" %os.listdir(os.getcwd())
